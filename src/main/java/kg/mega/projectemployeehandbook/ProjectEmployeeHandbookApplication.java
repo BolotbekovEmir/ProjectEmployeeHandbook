ebb5152afb28e7b040c11e31b65aa6f5dd12e2c2
Emir Bolotbekov <bolotbekove32@gmail.com>
2023-11-07 14:20:52 +0600

    Add Swagger UI

--- src/main/java/kg/mega/projectemployeehandbook/ProjectEmployeeHandbookApplication.java
+++ src/main/java/kg/mega/projectemployeehandbook/ProjectEmployeeHandbookApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjectEmployeeHandbookApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjectEmployeeHandbookApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectEmployeeHandbookApplication.class, args);
+    }
 
 }
