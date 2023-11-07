2be6eaac063c7889e7edd5cc0c27e232a8eaad08
Emir Bolotbekov <bolotbekove32@gmail.com>
2023-11-07 14:10:19 +0600

    Initial commit

---
+++ src/main/java/kg/mega/projectemployeehandbook/ProjectEmployeeHandbookApplication.java
@@ -0,0 +1,13 @@
+package kg.mega.projectemployeehandbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEmployeeHandbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectEmployeeHandbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kg/mega/projectemployeehandbook/ProjectEmployeeHandbookApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.mega.projectemployeehandbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectEmployeeHandbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
